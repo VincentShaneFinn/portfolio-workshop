ca3612478e04754c3025efe986f26f65bfe1a28f
VincentShaneFinn <vincent.s.finn@gmail.com>
2021-07-03 16:09:18 -0400

    put calculator in a class

--- src/__tests__/step_definitions/calculator.steps.ts
+++ src/__tests__/step_definitions/calculator.steps.ts
@@ -1,13 +1,5 @@
 import { defineFeature, loadFeature } from 'jest-cucumber';
-
-class Calculator {
-    static add(value1: number, value2: number): number {
-        return value1 + value2;
-    }
-    static multiply(value1: number, value2: number): number {
-        return value1 * value2;
-    }
-}
+import { Calculator } from '../../domain/calculator';
 
 const feature = loadFeature('src/__tests__/features/calculator.feature');
 

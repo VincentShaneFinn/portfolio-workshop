14ee9489d631047c8a3845580dc0a812cd919514
VincentShaneFinn <vincent.s.finn@gmail.com>
2021-07-04 20:53:55 -0400

    more functional and small refactors that are not big improvements

--- src/presentation/components/kanban-board.tsx
+++ src/presentation/components/kanban-board.tsx
@@ -20,6 +20,8 @@ class KanbanBoard extends Component<{}, KanbanBoardState> {
         closedList: [] as Array<string>
     };
 
+    //#region Card creation and movement
+
     addCard() {
         this.setState(prevState => ({
             openList: [...prevState.openList, prevState.cardTitle]
@@ -36,50 +38,65 @@ class KanbanBoard extends Component<{}, KanbanBoardState> {
         }));
     }
 
-    getOpenList() {
-        return this.state.openList.map((cardTitle, index) =>
+    //#endregion
+
+    //#region Rendering helpers
+    
+    renderList(list: Array<string>, hasCloseButton: boolean) {
+        let _this = this;
+        function conditionallyDrawCloseBtn(index: number) {
+            if (hasCloseButton) {
+                return (
+                    <div>
+                        <br />
+                        <button className="btn btn-primary close-kanban-card-btn" onClick={() => _this.moveCard(index)}>Close</button>
+                    </div>
+                )
+            }
+        }
+
+        return list.map((cardTitle, index) =>
             <div className="kanban-card" key={cardTitle}>
                 <div className="kanban-card-title">{cardTitle}</div>
-                <br />
-                <button className="btn btn-primary close-kanban-card-btn" onClick={() => this.moveCard(index)}>Close</button>
+                {conditionallyDrawCloseBtn(index)}
             </div>
         );
     }
 
-    getClosedList() {
-        return this.state.closedList.map((cardTitle) =>
-            <div className="kanban-card" key={cardTitle}>
-                <div className="kanban-card-title">{cardTitle}</div>
+    renderAddCard() {
+        return (
+            <div className="add-kanban-card">
+                <div className="form-group">
+                    <label className="kanban-card-title-label w-100">
+                        Title<br />
+                        <input className="kanban-card-title-input form-control"
+                            onChange={event => this.setState({ cardTitle: event.target.value })}
+                            onKeyPress={event => { if (event.key == "Enter") this.addCard(); }} ></input>
+                    </label>
+                </div>
+                <br />
+                <button className="btn btn-primary add-kanban-card-btn" onClick={this.addCard}>Add Card</button>
             </div>
-        );
+        )
     }
+
+    //#endregion
 
     render() {
         return (
             <div className="kanban-board container">
-                <div className="add-kanban-card">
-                    <div className="form-group">
-                        <label className="kanban-card-title-label w-100">
-                            Title<br />
-                            <input className="kanban-card-title-input form-control" 
-                            onChange={event => this.setState({ cardTitle: event.target.value })}
-                            onKeyPress={event => { if(event.key == "Enter") this.addCard(); }} ></input>
-                        </label>
-                    </div>
-                    <br />
-                    <button className="btn btn-primary add-kanban-card-btn" onClick={this.addCard}>Add Card</button>
-                </div>
+                {this.renderAddCard()}
                 <div className="row kanban-lists">
                     <div className="col-6">
                         <h3>Open</h3>
                         <div className="open-list">
-                            {this.getOpenList()}
+                            {this.renderList(this.state.openList, true)}
                         </div>
                     </div>
                     <div className="col-6">
                         <h3>Closed</h3>
                         <div className="closed-list">
-                            {this.getClosedList()}
+                            {this.renderList(this.state.closedList, false)}
                         </div>
                     </div>
                 </div>
